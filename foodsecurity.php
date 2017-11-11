0058ea97c6e8520dc53df361d613233474c442c9
Oroni Brian <brianoroni6@gmail.com>
2017-11-11 15:48:27 +0300

    Search added

--- foodsecurity.php
+++ foodsecurity.php
@@ -138,7 +138,21 @@
                                 <p class="category">For food security and livelihood</p>
                             </div>
                             <div class="content table-responsive table-full-width">
-                                <table class="table table-hover">
+                                <style type="text/css">
+                                    #myInput {
+                                    background-image: url('/css/searchicon.png'); /* Add a search icon to input */
+                                    background-position: 10px 12px; /* Position the search icon */
+                                    background-repeat: no-repeat; /* Do not repeat the icon image */
+                                    width: 100%; /* Full-width */
+                                    font-size: 16px; /* Increase font-size */
+                                    padding: 12px 20px 12px 40px; /* Add some padding */
+                                    border: 1px solid #ddd; /* Add a grey border */
+                                    margin-bottom: 12px; /* Add some space below the input */
+}
+                                </style>
+                                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+
+                                <table class="table table-hover" id="table">
                                     <thead>
                                         <th>Name</th>
                                     	<th>Location</th>
@@ -249,5 +263,28 @@
 	<!-- Paper Dashboard DEMO methods, don't include it in your project! -->
 	<script src="assets/js/demo.js"></script>
 
+
+    <script>
+    function myFunction() {
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+</script>
 
 </html>
